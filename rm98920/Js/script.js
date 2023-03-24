784632d91e87bd9ae448ecc01784a8e87ec3767f
Luan-Nijou <RM98920@fiap.com.br>
2023-03-23 21:18:19 -0300

    Utilização de evento

---
+++ rm98920/Js/script.js
@@ -0,0 +1,21 @@
+//alert('TESTE DE INICIO')
+
+//const nome1 = 'Mario';
+//var nome = "João";
+//if (nome != ""){
+    let nome = Maria;
+//}
+//Hoisting
+
+/*nome = joão;
+console.log(nome)
+var nome*/
+
+//Recuperando o elemento h1 através do id e colocando em uma constante
+const h1element = document.getElementById('meu-titulo')
+
+//Escutando o evento de click no elemtno h1 com a função
+//addeventListener
+h1element addEventListener("click",()=>{
+    alert('Agora com super-poderes!')
+})
