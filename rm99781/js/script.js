25f8498b2be92d53508d148ad28277f8dfd375fd
pbrnx <pedrobarone2709@gmail.com>
2023-03-30 20:42:21 -0300

    novos eventos de JS

--- rm99781/js/script.js
+++ rm99781/js/script.js
@@ -1,21 +1,34 @@
-//var h1Element = document.getElementById("meu-titulo");
+const h1Element = document.getElementById("meu-titulo");
 
 //Atrelando ao h1 um evento de click
 
-//h1Element.addEventListener("click", ()=>{
- //   alert("Hello World");
+h1Element.addEventListener("click", ()=>{
+    alert("Hello World");
 
-//});
+});
 
 //Através do evento de mouse que passa sobre o elemento
 //Altere a cor de fundo do elemento h1 e também a cor do texto!
 
-const h1Element = document.getElementById("meu-titulo");
 
 h1Element.addEventListener("mouseover", ()=>{
 
     h1Element.style.backgroundColor = "red";
-    
-    
+    h1Element.style.fontSize = "64px";
+    h1Element.style.color = "white";
+
 });
+
+//Quando o mouse sai do bloco, volta ao default
+h1Element.addEventListener("mouseleave",()=>{
+    h1Element.style.backgroundColor = "White";
+    h1Element.style.fontSize = "32px";
+    h1Element.style.color = "black";
+});
+
+
+//Operador ternário (?)
+
+
+
 
