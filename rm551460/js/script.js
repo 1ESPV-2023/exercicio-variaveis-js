c8edc190dc00db6973419f561e07e4f4e0d3a83c
Robertooan07 <RM551460@fiap.com.br>
2023-03-30 20:59:00 -0300

    Par ou impar

--- rm551460/js/script.js
+++ rm551460/js/script.js
@@ -17,9 +17,22 @@ h1Element.addEventListener("mouseover", ()=>{
 
 })
 
+// no código abaixo é quando tirar o mouse de cima do texto e ele muda de cor
 h1Element.addEventListener("mouseleave", ()=>{
     h1Element.style.backgroundColor = "white";
     h1Element.style.fontSize = "32px";
     h1Element.style.color = "black";
 
 })
+
+
+
+// OPERADR TERNÁRIO ( ? ) VAMOS MOSTRAR SE UM NUMERO É PAR OU IMPAR
+
+let num1 = 10;
+let mum2 = 5;
+
+let resultado = !(10%5) ? "PAR" : "IMPAR";
+//Apresentando o resultado com interpolação!
+console.log(`O número ${num1} é ${resultado}.`)
+
