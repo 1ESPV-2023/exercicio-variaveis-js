a60c6c7522ca0165a345731fb5ca38f5dd5505ac
TonyWillianFIAP <rm550667@fiap.com.br>
2023-03-30 20:54:31 -0300

    Aula 30/3 pt.4

--- rm550667/js/script.js
+++ rm550667/js/script.js
@@ -3,6 +3,25 @@ console.log('Alô galera da bagunça')
 const h1Element = document.getElementById('Batata Gigante');
 
 //Atrelando um evento de click ao h1
-h1Element.addEventListener('click',()=>{
-    alert('Agora com mais batata')
+// h1Element.addEventListener('click',()=>{
+//    alert('Agora com mais batata')
+//})
+h1Element.addEventListener('mouseover',()=>{
+    h1Element.style.backgroundColor = 'red';
+    h1Element.style.color = 'blue';
+    h1Element.style.fontSize = '64px'
 })
+h1Element.addEventListener('mouseleave',()=>{
+    h1Element.style.backgroundColor = 'white';
+    h1Element.style.color = 'black';
+    h1Element.style.fontSize = '32px'
+})
+
+//OPERADOR TERNÁRIO (?????????)
+//Realizar uma operação de paridade utilizando o operador módulo (%)
+let num1 = 10;
+let num2 = 5;
+
+let resultado = !(10%5) ? 'PAR' : 'IMPAR';
+//Apresentando o resultado com interpolação
+console.log (`O número é ${num1} é ${resultado}.`)
