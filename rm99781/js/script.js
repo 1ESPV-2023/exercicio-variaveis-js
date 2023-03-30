240e93048c6ec54f6782c057fd90522c898c7bff
pbrnx <pedrobarone2709@gmail.com>
2023-03-30 20:29:57 -0300

    alterações jS

--- rm99781/js/script.js
+++ rm99781/js/script.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+//var h1Element = document.getElementById("meu-titulo");
+
+//Atrelando ao h1 um evento de click
+
+//h1Element.addEventListener("click", ()=>{
+ //   alert("Hello World");
+
+//});
+
+//Através do evento de mouse que passa sobre o elemento
+//Altere a cor de fundo do elemento h1 e também a cor do texto!
+
+const h1Element = document.getElementById("meu-titulo");
+
+h1Element.addEventListener("mouseover", ()=>{
+
+    h1Element.style.backgroundColor = "red";
+    
+    
+});
+
