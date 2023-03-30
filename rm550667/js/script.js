b80f304915926a19c8960ec93fa1121d400085a2
TonyWillianFIAP <rm550667@fiap.com.br>
2023-03-30 20:11:36 -0300

    Aula 30/3 pt.3

--- rm550667/js/script.js
+++ rm550667/js/script.js
@@ -1,1 +1,8 @@
 console.log('Alô galera da bagunça')
+
+const h1Element = document.getElementById('Batata Gigante');
+
+//Atrelando um evento de click ao h1
+h1Element.addEventListener('click',()=>{
+    alert('Agora com mais batata')
+})
